6c49bed5a8afcdf34f3c68c01ce8221faa802468
B Kanhu Charan <iamkanhuc@gmail.com>
2021-06-19 11:02:31 +0530

    Add option to remove comments counts

--- chrome/options.js
+++ chrome/options.js
@@ -1,17 +1,18 @@
 // Saves options to chrome.storage
 function save_options() {
-  var feedWidth = document.getElementById("feed-width").value;
-  var showLatest = document.getElementById("latest").checked;
-  var centerNavigation = document.getElementById("center-navigation").checked;
-  var noTweetButton = document.getElementById("tweet").checked;
-  var feedBorders = document.getElementById("feed-borders").checked;
-  var noBorders = document.getElementById("borders").checked;
-  var noLikes = document.getElementById("like").checked;
-  var noRetweets = document.getElementById("retweet").checked;
-  var noExploreButton = document.getElementById("explore").checked;
-  var noNotificationsButton = document.getElementById("notifications").checked;
-  var noBookmarksButton = document.getElementById("bookmarks").checked;
-  var noListsButton = document.getElementById("lists").checked;
+  var feedWidth = document.getElementById('feed-width').value;
+  var showLatest = document.getElementById('latest').checked;
+  var centerNavigation = document.getElementById('center-navigation').checked;
+  var noTweetButton = document.getElementById('tweet').checked;
+  var feedBorders = document.getElementById('feed-borders').checked;
+  var noBorders = document.getElementById('borders').checked;
+  var noLikes = document.getElementById('like').checked;
+  var noRetweets = document.getElementById('retweet').checked;
+  var noComment = document.getElementById('comment').checked;
+  var noExploreButton = document.getElementById('explore').checked;
+  var noNotificationsButton = document.getElementById('notifications').checked;
+  var noBookmarksButton = document.getElementById('bookmarks').checked;
+  var noListsButton = document.getElementById('lists').checked;
   chrome.storage.sync.set(
     {
       feedWidth: feedWidth,
@@ -22,6 +23,7 @@ function save_options() {
       noBorders: noBorders,
       noLikes: noLikes,
       noRetweets: noRetweets,
+      noComment: noComment,
       noExploreButton: noExploreButton,
       noNotificationsButton: noNotificationsButton,
       noBookmarksButton: noBookmarksButton,
@@ -29,10 +31,10 @@ function save_options() {
     },
     function () {
       // Update status to let user know options were saved.
-      var status = document.getElementById("status");
-      status.textContent = "Options saved.";
+      var status = document.getElementById('status');
+      status.textContent = 'Options saved.';
       setTimeout(function () {
-        status.textContent = "";
+        status.textContent = '';
       }, 750);
     }
   );
@@ -43,7 +45,7 @@ function save_options() {
 function restore_options() {
   chrome.storage.sync.get(
     {
-      feedWidth: "700",
+      feedWidth: '700',
       showLatest: false,
       centerNavigation: false,
       noTweetButton: false,
@@ -51,28 +53,30 @@ function restore_options() {
       noBorders: false,
       noLikes: false,
       noRetweets: false,
+      noComment: false,
       noExploreButton: false,
       noNotificationsButton: false,
       noBookmarksButton: false,
       noListsButton: false,
     },
     function (items) {
-      document.getElementById("feed-width").value = items.feedWidth;
-      document.getElementById("latest").checked = items.showLatest;
-      document.getElementById("center-navigation").checked =
+      document.getElementById('feed-width').value = items.feedWidth;
+      document.getElementById('latest').checked = items.showLatest;
+      document.getElementById('center-navigation').checked =
         items.centerNavigation;
-      document.getElementById("tweet").checked = items.noTweetButton;
-      document.getElementById("feed-borders").checked = items.feedBorders;
-      document.getElementById("borders").checked = items.noBorders;
-      document.getElementById("like").checked = items.noLikes;
-      document.getElementById("retweet").checked = items.noRetweets;
-      document.getElementById("explore").checked = items.noExploreButton;
-      document.getElementById("notifications").checked =
+      document.getElementById('tweet').checked = items.noTweetButton;
+      document.getElementById('feed-borders').checked = items.feedBorders;
+      document.getElementById('borders').checked = items.noBorders;
+      document.getElementById('like').checked = items.noLikes;
+      document.getElementById('retweet').checked = items.noRetweets;
+      document.getElementById('comment').checked = items.noComment;
+      document.getElementById('explore').checked = items.noExploreButton;
+      document.getElementById('notifications').checked =
         items.noNotificationsButton;
-      document.getElementById("bookmarks").checked = items.noBookmarksButton;
-      document.getElementById("lists").checked = items.noListsButton;
+      document.getElementById('bookmarks').checked = items.noBookmarksButton;
+      document.getElementById('lists').checked = items.noListsButton;
     }
   );
 }
-document.addEventListener("DOMContentLoaded", restore_options);
-document.getElementById("save").addEventListener("click", save_options);
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
