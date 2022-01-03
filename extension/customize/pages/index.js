252d6a7681c372f6477d844c4bb5a4e15468f79b
Thomas Wang <wang@hey.com>
2022-01-02 23:44:20 -0800

    comments and cleanup

--- extension/customize/pages/index.js
+++ extension/customize/pages/index.js
@@ -3,14 +3,12 @@ import Header from "../components/layout/Header"
 import Main from "../components/layout/Main"
 import Footer from "../components/layout/Footer"
 
-const IndexPage = () => {
-  return (
-    <Container>
-      <Header />
-      <Main />
-      <Footer />
-    </Container>
-  )
-}
+const IndexPage = () => (
+  <Container>
+    <Header />
+    <Main />
+    <Footer />
+  </Container>
+)
 
 export default IndexPage
