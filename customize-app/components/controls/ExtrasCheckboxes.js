ba4a4c6b6b48341296aa14808313516318846c89
noops <redtinfoilcrane@gmail.com>
2022-02-13 03:16:05 -0800

    add checkbox component

--- customize-app/components/controls/ExtrasCheckboxes.js
+++ customize-app/components/controls/ExtrasCheckboxes.js
@@ -528,3 +528,50 @@ export const CheckboxAlwaysShowLatest = () => {
     </div>
   )
 }
+
+export const CheckboxHideTitleNotifications = () => {
+  const [userTitle, setUserTitle] = useState(false)
+
+  useEffect(() => {
+    const getUserDefaultTitle = async () => {
+      try {
+        const userDefaultTitle = await getStorage("titleNotifications")
+        userDefaultTitle &&
+          setUserTitle(userDefaultTitle === "off" ? true : false)
+      } catch (error) {
+        console.warn(error)
+      }
+    }
+
+    getUserDefaultTitle()
+  }, [])
+
+  return (
+    <div className="flex items-center justify-between w-full py-1">
+      <label htmlFor="titleNotifications" className="text-base tracking-normal">
+        Hide Title Notification Count*
+      </label>
+      <div className="grid rounded-full cursor-pointer w-9 h-9 place-items-center hover:bg-twitterAccentFour">
+        <StyledCheckbox
+          onCheckedChange={async (checked) => {
+            setUserTitle(checked)
+            try {
+              await setStorage({
+                titleNotification: checked ? "off" : "on"
+              })
+            } catch (error) {
+              console.warn(error)
+            }
+          }}
+          checked={userTitle}
+          id="titleNotifications"
+          className="flex items-center justify-center w-5 h-5 rounded-[4px] bg-twitterAccentThree"
+        >
+          <CheckboxPrimitive.Indicator className="text-white">
+            <CheckIcon />
+          </CheckboxPrimitive.Indicator>
+        </StyledCheckbox>
+      </div>
+    </div>
+  )
+}
