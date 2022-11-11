7f4e1aa2a3f16244c3834f6111b3509af555c384
Fabrizio Rinaldi <fabrizio@typefully.com>
2022-11-11 10:08:24 +0000

    Remove update screen for now
    It needs more work

--- extension/content/background.js
+++ extension/content/background.js
@@ -6,9 +6,4 @@ chrome.runtime.onInstalled.addListener((object) => {
       url: "https://typefully.com/minimal-twitter/welcome",
     });
   }
-  if (object.reason === "update") {
-    chrome.tabs.create({
-      url: "https://typefully.com/minimal-twitter/welcome?updated=true",
-    });
-  }
 });
