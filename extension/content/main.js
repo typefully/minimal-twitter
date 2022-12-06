939a5002a8465edcc57e885824cc8d7322c35bdd
Thomas Wang <wang@hey.com>
2022-12-06 12:38:11 +0900

    move

--- extension/content/main.js
+++ extension/content/main.js
@@ -292,6 +292,8 @@ const addListsButton = () => {
       profileNodeClone.removeAttribute("data-testid");
       profileNodeClone.firstChild.firstChild.firstChild.innerHTML = `<g><path d="M3 4.5C3 3.12 4.12 2 5.5 2h13C19.88 2 21 3.12 21 4.5v15c0 1.38-1.12 2.5-2.5 2.5h-13C4.12 22 3 20.88 3 19.5v-15zM5.5 4c-.28 0-.5.22-.5.5v15c0 .28.22.5.5.5h13c.28 0 .5-.22.5-.5v-15c0-.28-.22-.5-.5-.5h-13zM16 10H8V8h8v2zm-8 2h8v2H8v-2z"></path></g>`;
       profileNodeClone.firstChild.lastChild.firstChild.innerText = "Lists";
+      profileNode.insertAdjacentElement("beforebegin", profileNodeClone);
+
       // add custom hover state background
       addStyles(
         "mt-listsButtonNode",
@@ -301,7 +303,6 @@ const addListsButton = () => {
         }
         `
       );
-      profileNode.insertAdjacentElement("beforebegin", profileNodeClone);
     }
   }
 };
