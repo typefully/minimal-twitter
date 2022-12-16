7aebd61a3bbd18435d914ed73f84721fb9865d2f
Fabrizio Rinaldi <fabrizio@typefully.com>
2022-12-16 10:42:32 +0100

    Refactor

--- content-scripts/src/modules/options/all.js
+++ content-scripts/src/modules/options/all.js
@@ -35,96 +35,65 @@ import { changeWriterMode } from "./writer-mode";
 
 // Array of user preferences, passed to `injectAllChanges`
 export const userPreferences = [
-  "timelineWidth", // 1
-  "timelineBorders", // 2
-  "homeButton", // 3
-  "exploreButton", //
-  "notificationsButton", //
-  "messagesButton", //
-  "bookmarksButton", //
-  "topArticlesButton", //
-  "communitiesButton", //
-  "listsButton", //
+  "timelineWidth",
+  "timelineBorders",
+  "tweetBorders",
+  "homeButton",
+  "exploreButton",
+  "notificationsButton",
+  "messagesButton",
+  "bookmarksButton",
+  "topArticlesButton",
+  "communitiesButton",
+  "listsButton",
   "profileButton",
-  "navigationButtonsLabelsHover", //
-  "navigationButtonsLabels", //
-  "navigationCenter", //
-  "writerMode", //
-  "replyCount", //
-  "retweetCount", //
-  "likeCount", //
-  "followCount", //
-  "tweetButton", //
-  "searchBar", //
-  "transparentSearch", //
-  "removePromotedPosts", //
-  "whoToFollow", //
-  "topicsToFollow", //
-  "latestTweets", //
-  "tweetBorders", //
-  "typefullyGrowTab", //
-  "typefullyComposerButtons", //
+  "navigationButtonsLabelsHover",
+  "navigationButtonsLabels",
+  "navigationCenter",
+  "writerMode",
+  "replyCount",
+  "retweetCount",
+  "likeCount",
+  "followCount",
+  "tweetButton",
+  "searchBar",
+  "transparentSearch",
+  "removePromotedPosts",
+  "whoToFollow",
+  "topicsToFollow",
+  "typefullyGrowTab",
+  "latestTweets",
+  "typefullyComposerButtons",
 ];
 
 export const injectAllChanges = (data) => {
-  const {
-    timelineWidth, // 1
-    timelineBorders, // 2
-    homeButton, // 3
-    exploreButton, //
-    notificationsButton, //
-    messagesButton, //
-    bookmarksButton, //
-    topArticlesButton, //
-    communitiesButton, //
-    listsButton, //
-    profileButton,
-    navigationButtonsLabelsHover, //
-    navigationButtonsLabels, //
-    navigationCenter, //
-    writerMode, //
-    replyCount, //
-    retweetCount, //
-    likeCount, //
-    followCount, //
-    tweetButton, //
-    searchBar, //
-    transparentSearch, //
-    removePromotedPosts, //
-    whoToFollow, //
-    topicsToFollow, //
-    latestTweets, //
-    tweetBorders, //
-    typefullyGrowTab, //
-    typefullyComposerButtons, //
-  } = data;
-  changeTimelineWidth(timelineWidth); // Timeline Width
-  changeTimelineBorders(timelineBorders); // Timeline Borders
-  changeTweetBorders(tweetBorders); // Timeline Borders
-  changeHomeButton(homeButton); // Home Button
-  changeExploreButton(exploreButton); // Explore Button
-  changeNotificationsButton(notificationsButton); // Notification Button
-  changeMessagesButton(messagesButton); // Messages Button
-  changeBookmarksButton(bookmarksButton); // Bookmarks Button
-  changeTopArticlesButton(topArticlesButton); // Top Articles Button
-  changeCommunitiesButton(communitiesButton); // Communities Button
-  changeListsButton(listsButton); // Lists Button
-  changeGrowButton(typefullyGrowTab); // Typefully Grow Button
-  changeTypefullyComposerButtons(typefullyComposerButtons); // Typefully Composer Buttons
-  changeProfileButton(profileButton); // Profile Button
-  changeNavigationButtonsLabelsHover(navigationButtonsLabelsHover); // Navigation Buttons Labels on Hover
-  changeNavigationButtonsLabels(navigationButtonsLabels); // Navigation Buttons Labels
-  changeNavigationCenter(navigationCenter); // Center Navigation
-  changeWriterMode(writerMode); // Writer Mode
-  changeReplyCount(replyCount); // Hide Reply Count
-  changeRetweetCount(retweetCount); // Hide Retweet Count
-  changeLikeCount(likeCount); // Hide Like Count
-  changeFollowCount(followCount); // Hide Follow
-  changeTweetButton(tweetButton); // Hide Tweet Button
-  changeSearchBar1(searchBar); // Hide Search Bar
-  changeSearchBar2(transparentSearch); // Transparent Search Bar
-  changePromotedPosts(removePromotedPosts); // Remove Promoted Posts
-  changeWhoToFollow(whoToFollow); // Who to Follow
-  changeTopicsToFollow(topicsToFollow); // Topics to Follow
-  changeLatestTweets(latestTweets); // Always Show Latest Tweets
+  changeTimelineWidth(data?.timelineWidth); // Timeline Width
+  changeTimelineBorders(data?.timelineBorders); // Timeline Borders
+  changeTweetBorders(data?.tweetBorders); // Timeline Borders
+  changeHomeButton(data?.homeButton); // Home Button
+  changeExploreButton(data?.exploreButton); // Explore Button
+  changeNotificationsButton(data?.notificationsButton); // Notification Button
+  changeMessagesButton(data?.messagesButton); // Messages Button
+  changeBookmarksButton(data?.bookmarksButton); // Bookmarks Button
+  changeTopArticlesButton(data?.topArticlesButton); // Top Articles Button
+  changeCommunitiesButton(data?.communitiesButton); // Communities Button
+  changeListsButton(data?.listsButton); // Lists Button
+  changeProfileButton(data?.profileButton); // Profile Button
+  changeNavigationButtonsLabelsHover(data?.navigationButtonsLabelsHover); // Navigation Buttons Labels on Hover
+  changeNavigationButtonsLabels(data?.navigationButtonsLabels); // Navigation Buttons Labels
+  changeNavigationCenter(data?.navigationCenter); // Center Navigation
+  changeWriterMode(data?.writerMode); // Writer Mode
+  changeReplyCount(data?.replyCount); // Hide Reply Count
+  changeRetweetCount(data?.retweetCount); // Hide Retweet Count
+  changeLikeCount(data?.likeCount); // Hide Like Count
+  changeFollowCount(data?.followCount); // Hide Follow
+  changeTweetButton(data?.tweetButton); // Hide Tweet Button
+  changeSearchBar1(data?.searchBar); // Hide Search Bar
+  changeSearchBar2(data?.transparentSearch); // Transparent Search Bar
+  changePromotedPosts(data?.removePromotedPosts); // Remove Promoted Posts
+  changeWhoToFollow(data?.whoToFollow); // Who to Follow
+  changeTopicsToFollow(data?.topicsToFollow); // Topics to Follow
+  changeGrowButton(data?.typefullyGrowTab); // Typefully Grow Button
+  changeLatestTweets(data?.latestTweets); // Always Show Latest Tweets
+  changeTypefullyComposerButtons(data?.typefullyComposerButtons); // Typefully Composer Buttons
 };
