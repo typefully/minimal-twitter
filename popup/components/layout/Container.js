6313f25595b5ad8758c3350fdbed6db7aec76a00
Thomas Wang <wang@hey.com>
2022-12-16 15:44:18 +0700

    max width popup

--- popup/components/layout/Container.js
+++ popup/components/layout/Container.js
@@ -1,5 +1,5 @@
 const Container = ({ children }) => (
-  <div className="relative flex justify-center w-full">
+  <div className="relative flex justify-center w-full max-w-[420px]">
     <div className="relative overflow-scroll">
       <div className="relative flex flex-col p-1 font-sans dark:text-white text-black font-normal min-w-[420px] w-full">
         {children}
