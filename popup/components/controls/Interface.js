5407d75f8a265bb38df99e4a9c11f1a2db3f388b
Thomas Wang <wang@hey.com>
2023-01-15 21:29:32 -0800

    move inter font first

--- popup/components/controls/Interface.js
+++ popup/components/controls/Interface.js
@@ -4,6 +4,7 @@ const Interface = () => (
   <form className="flex flex-col items-center justify-between px-4 dark:bg-twitterBgTwoDark bg-twitterBgTwo rounded-2xl">
     <div className="w-full py-4">
       <div className="flex flex-col gap-y-4">
+        <SwitchControl label="Inter Font" storageKey="interFont" />
         <SwitchControl
           label="Search Bar"
           storageKey="searchBar"
@@ -18,7 +19,6 @@ const Interface = () => (
           storageKey="tweetButton"
           defaultState={true}
         />
-        <SwitchControl label="Inter Font" storageKey="interFont" />
         <SwitchControl
           label="Notifications in Title"
           storageKey="titleNotifications"
