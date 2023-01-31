9e5eba497228e6dca0466eb03c8d8d4e1e957688
Thomas Wang <wang@hey.com>
2023-01-31 01:49:48 -0800

    fix

--- popup/components/layout/Container.js
+++ popup/components/layout/Container.js
@@ -1,5 +1,5 @@
 const Container = ({ children }) => (
-  <div className="relative flex flex-col p-1 font-sans dark:text-white text-black font-normal max-w-full w-[420px] w-full">
+  <div className="relative flex flex-col p-1 font-sans dark:text-white text-black font-normal max-w-full w-[420px]">
     {children}
   </div>
 )
