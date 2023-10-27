f2e39538391ceabd97294f5f200043dd25575401
Fabrizio Rinaldi <linuz90@gmail.com>
2023-10-27 10:44:28 +0100

    Copy change

--- popup/components/sections/TypefullySection.js
+++ popup/components/sections/TypefullySection.js
@@ -6,7 +6,7 @@ const TypefullySection = () => (
   <section className="flex flex-col gap-y-2">
     <SectionLabel htmlFor="user-control-typefully">Typefully</SectionLabel>
     <ControlsWrapper id="user-control-typefully">
-      <SwitchControl label="Composer Buttons" storageKey="typefullyComposerButtons" />
+      <SwitchControl label={`"Save to Typefully" in composers`} storageKey="typefullyComposerButtons" />
     </ControlsWrapper>
   </section>
 );
