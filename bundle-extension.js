ad2d5f62fb2f54c0b3d2a44f74b93edf70e5ed51
Fabrizio Rinaldi <linuz90@gmail.com>
2024-08-06 15:18:52 +0100

    Bump version

--- bundle-extension.js
+++ bundle-extension.js
@@ -20,7 +20,7 @@ let manifest = {
   name: "Minimal Theme for Twitter / X",
   short_name: "Minimal Twitter",
   description: "Refine and declutter the 𝕏/Twitter web experience.",
-  version: "6.1.0",
+  version: "6.1.1",
   icons: {
     16: "images/MinimalTwitterIcon16.png",
     32: "images/MinimalTwitterIcon32.png",
