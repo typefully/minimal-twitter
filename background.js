8263aa22a51676f0f2e38c8ed727d586e93bb474
Fabrizio Rinaldi <linuz90@gmail.com>
2025-01-30 15:57:19 +0000

    Open update page when extension is updated

--- background.js
+++ background.js
@@ -1,10 +1,20 @@
 // Used by the manifest v3 extension
 
 chrome.runtime.onInstalled.addListener((object) => {
-  if (object.reason === "install") {
+  if (object.reason !== "install" && object.reason !== "update") {
+    return;
+  }
+
+  const targetUrl = `https://typefully.com/minimal-twitter/welcome${
+    object.reason === "update" ? "?updated=true" : ""
+  }`;
+
+  if (targetUrl) {
     chrome.tabs.create({
-      url: "https://typefully.com/minimal-twitter/welcome",
+      url: targetUrl,
     });
+
+    // Reload any open Twitter/X tabs
     chrome.tabs.query({ url: "*://twitter.com/*" }, (tabs) => {
       tabs.forEach((tab) => {
         chrome.tabs.reload(tab.id);
